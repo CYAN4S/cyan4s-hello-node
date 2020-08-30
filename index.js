e46b6720f0eeaeabcded9cb6e649ec67b66bd385
CYAN4S <37798132+CYAN4S@users.noreply.github.com>
2020-08-30 17:20:15 +0900

    Add cookie system

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const http = require("http");
 const fs = require("fs");
 const PORT = process.env.PORT || 5000;
 
+const { parseCookies } = require("./parseCookies");
+
 const server = http.createServer((req, res) => {
+  const cookies = parseCookies(req.headers.cookie);
+  console.log(req.url, cookies);
+  res.writeHead(200, {'Set-Cookie': 'mycookie=test'});
+
   fs.readFile("./index.html", (err, data) => {
     if (err) {
       throw err;
